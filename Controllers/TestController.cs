fac489d4b89cc4f1ac1733fc182784d5f86c8a94
duong mau <dpmau.98@gmail.com>
2020-08-28 12:38:56 +0700

    fix query

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -17,17 +17,22 @@ namespace _7A_Api.Controllers
     public IActionResult Index()
     {
 
-      var tintuc = from tt in _context.TableTintuc select new { tt.Id_Lv0, name = tt.Ten_Vi, slug = tt.Tenkhongdau_Vi };
-      var tinTucList = from ttl in _context.TableTintucLists select new { ttl.Id, name = ttl.Ten_Vi, slug = ttl.Tenkhongdau_Vi };
+      var tintuc = (from tt in _context.TableTintuc
+                    select new { tt.Id_Lv0, name = tt.Ten_Vi, slug = tt.Tenkhongdau_Vi })
+                   .ToList();
+      var tinTucList = (from ttl in _context.TableTintucLists
+                        select new { ttl.Id, name = ttl.Ten_Vi, slug = ttl.Tenkhongdau_Vi, ttl.Type })
+                       .ToList();
 
-      var query = from ttl in tinTucList.ToList()
-                  join tt in tintuc.ToList() on ttl.Id equals tt.Id_Lv0
-                  into grouped
+      var query = from ttl in tinTucList
+                  join tt in tintuc on ttl.Id equals tt.Id_Lv0
+                  into joined
+                  where ttl.Type == "ky-thuat"
                   select new
                   {
                     Name = ttl.name,
                     Slug = ttl.slug,
-                    News = from item in grouped select new { item.name, item.slug }
+                    News = from item in joined select new { item.name, item.slug }
                   };
 
       return Ok(new
