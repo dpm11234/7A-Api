8ad470dd74e69b0c5bf6c620df2e93c16001d542
cko-dev <thongnguyen.uit@gmail.com>
2020-09-01 11:44:23 +0700

    add media

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -2,7 +2,6 @@ using H7A_Api.Models;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-// using System.Data.Entity;
 
 namespace H7A_Api.Controllers
 {
@@ -14,9 +13,6 @@ namespace H7A_Api.Controllers
         public ApiController(AppDbContext context)
         {
             _context = context;
-
-            // _context.Database. = s => System.Diagnostics.Debug.WriteLine(s);
-
         }
 
         [HttpGet("categories")]
@@ -42,6 +38,7 @@ namespace H7A_Api.Controllers
                             children = from item in joined select new { item.name, item.slug }
                         };
 
+
             return Ok(new
             {
                 news = query.ToList()
@@ -65,15 +62,19 @@ namespace H7A_Api.Controllers
         [HttpGet("media")]
         public IActionResult GetAllMedia()
         {
-            return Ok("Media");
+              var result = _context.TableTintuc.Where(tt => tt.Hienthi == true && tt.Type == "thong-tin-truyen-thong")
+                                             .OrderByDescending((tt) => tt.Noibat)
+                                             .ThenByDescending((tt) => tt.Ngaysua)
+                                             .Select(tt => new { id = tt.Id, name = tt.Ten_Vi, slug = tt.Tenkhongdau_Vi, createdAt = tt.Ngaytao, pop = tt.Noibat })
+                                             .ToList();
+
+            return Ok(result);
         }
 
         [HttpGet("article/{id}")]
         public async Task<ActionResult> GetArticleDetail(uint id)
         {
-            // System.Console.WriteLine(slug);
             var result = await _context.TableTintuc.FindAsync(id);
-            // return Ok("Article");
             if (result == null || result.Hienthi == false)
             {
                 return NotFound(new { message = "article not found" });
