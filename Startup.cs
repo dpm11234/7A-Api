6c308c71403ea2dcd78dabd59a2b027a5eebd8e9
cko-dev <thongnguyen.uit@gmail.com>
2020-09-01 11:31:20 +0700

    use loggin

--- Startup.cs
+++ Startup.cs
@@ -10,45 +10,51 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using H7A_Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace H7A_Api
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-    private readonly IHostEnvironment _env;
-
-    public Startup(IConfiguration config, IHostEnvironment env)
+    public class Startup
     {
-      _config = config;
-      _env = env;
+        private readonly IConfiguration _config;
+        private readonly IHostEnvironment _env;
+
+        public Startup(IConfiguration config, IHostEnvironment env)
+        {
+            _config = config;
+            _env = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                if (_env.IsDevelopment())
+                {
+                    options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+                }
+
+                options.UseMySql(_config.GetConnectionString("Default"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
     }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddControllers();
-
-      services.AddDbContext<AppDbContext>(options =>
-      {
-        options.UseMySql(_config.GetConnectionString("Default"));
-      });
-    }
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapDefaultControllerRoute();
-      });
-    }
-  }
 }
