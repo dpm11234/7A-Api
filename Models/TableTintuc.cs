540f9a7b2c057d6264ac9e2d4c17071d7723acad
cko-dev <thongnguyen.uit@gmail.com>
2020-09-01 11:30:41 +0700

    change types

--- Models/TableTintuc.cs
+++ Models/TableTintuc.cs
@@ -4,50 +4,50 @@ using Microsoft.EntityFrameworkCore;
 
 namespace H7A_Api.Models
 {
-  public partial class TableTintuc
-  {
-    public uint Id { get; set; }
-    public int Noibat { get; set; }
-    public string Photo { get; set; }
-    public string Thumb { get; set; }
-    public string Ten_Vi { get; set; }
-    public string Ten_En { get; set; }
-    public string Ten_Ta { get; set; }
-    public string Ten_Ja { get; set; }
-    public string Tenkhongdau_Vi { get; set; }
-    public string Tenkhongdau_En { get; set; }
-    public string Tenkhongdau_Ta { get; set; }
-    public string Tenkhongdau_Ja { get; set; }
-    public string Mota_Vi { get; set; }
-    public string Mota_En { get; set; }
-    public string Mota_Ta { get; set; }
-    public string Mota_Ja { get; set; }
-    public string Noidung_Vi { get; set; }
-    public string Noidung_En { get; set; }
-    public string Noidung_Ta { get; set; }
-    public string Noidung_Ja { get; set; }
-    public string Title_Vi { get; set; }
-    public string Title_En { get; set; }
-    public string Title_Ta { get; set; }
-    public string Title_Ja { get; set; }
-    public string Keywords_Vi { get; set; }
-    public string Keywords_En { get; set; }
-    public string Keywords_Ta { get; set; }
-    public string Keywords_Ja { get; set; }
-    public string Description_Vi { get; set; }
-    public string Description_En { get; set; }
-    public string Description_Ta { get; set; }
-    public string Description_Ja { get; set; }
-    public uint Stt { get; set; }
-    public bool? Hienthi { get; set; }
-    public int Ngaytao { get; set; }
-    public int Ngaysua { get; set; }
-    public int Luotxem { get; set; }
-    public string Type { get; set; }
-    public int Id_Lv0 { get; set; }
-    public int Id_Lv1 { get; set; }
-    public int Id_Lv2 { get; set; }
-    public int Id_Lv3 { get; set; }
-    public int Id_Lv4 { get; set; }
-  }
+    public partial class TableTintuc
+    {
+        public uint Id { get; set; }
+        public int Noibat { get; set; }
+        public string Photo { get; set; }
+        public string Thumb { get; set; }
+        public string Ten_Vi { get; set; }
+        public string Ten_En { get; set; }
+        public string Ten_Ta { get; set; }
+        public string Ten_Ja { get; set; }
+        public string Tenkhongdau_Vi { get; set; }
+        public string Tenkhongdau_En { get; set; }
+        public string Tenkhongdau_Ta { get; set; }
+        public string Tenkhongdau_Ja { get; set; }
+        public string Mota_Vi { get; set; }
+        public string Mota_En { get; set; }
+        public string Mota_Ta { get; set; }
+        public string Mota_Ja { get; set; }
+        public string Noidung_Vi { get; set; }
+        public string Noidung_En { get; set; }
+        public string Noidung_Ta { get; set; }
+        public string Noidung_Ja { get; set; }
+        public string Title_Vi { get; set; }
+        public string Title_En { get; set; }
+        public string Title_Ta { get; set; }
+        public string Title_Ja { get; set; }
+        public string Keywords_Vi { get; set; }
+        public string Keywords_En { get; set; }
+        public string Keywords_Ta { get; set; }
+        public string Keywords_Ja { get; set; }
+        public string Description_Vi { get; set; }
+        public string Description_En { get; set; }
+        public string Description_Ta { get; set; }
+        public string Description_Ja { get; set; }
+        public uint Stt { get; set; }
+        public bool? Hienthi { get; set; }
+        public int Ngaytao { get; set; }
+        public int Ngaysua { get; set; }
+        public int Luotxem { get; set; }
+        public string Type { get; set; }
+        public int Id_Lv0 { get; set; }
+        public int Id_Lv1 { get; set; }
+        public int Id_Lv2 { get; set; }
+        public int Id_Lv3 { get; set; }
+        public int Id_Lv4 { get; set; }
+    }
 }
